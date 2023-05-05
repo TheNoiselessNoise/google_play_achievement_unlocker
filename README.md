82f861958b154a158db7b4f749228bbf366d48ad
TheNoiselessNoise <iceman980000@gmail.com>
2023-05-05 03:18:12 +0200

    nicecli unlocking achievements

--- README.md
+++ README.md
@@ -15,40 +15,38 @@ python gpau.py
 ### Example
 _unlock all 'secret' achievements in provided app_
 ```bash
-python3 gpau.py --list-sec-achs --unlock-listed -a com.grumpyrhinogames.necromerger
+python gpau.py --list-sec-achs --unlock-listed -a com.miniclip.plagueinc
 ```
-```
-CgkI0IOM87sIEAIQQA : Undead Alien I : Discover a lvl 1 Undead Alien. : 500xp
-CgkI0IOM87sIEAIQQQ : Undead Alien II : Discover a max lvl Undead Alien. : 500xp
-CgkI0IOM87sIEAIQQg : Guzzler I : Discover a Guzzler. : 500xp
-CgkI0IOM87sIEAIQQw : Peaceful Spirit I : Discover a lvl 1 Peaceful Spirit. : 500xp
-CgkI0IOM87sIEAIQRA : Peaceful Spirit II : Discover a max lvl Peaceful Spirit. : 500xp
-CgkI0IOM87sIEAIQRQ : Vengeful Spirit I : Discover a lvl 1 Vengeful Spirit. : 500xp
-CgkI0IOM87sIEAIQRg : Vengeful Spirit II : Discover a max lvl Vengeful Spirit. : 500xp
-CgkI0IOM87sIEAIQRw : Shield Bot : Discover the Shield Bot. : 500xp
-CgkI0IOM87sIEAIQSA : Soul Stalker : Discover the Soul Stalker. : 500xp
-Unlocking achievement CgkI0IOM87sIEAIQQA (com.grumpyrhinogames.necromerger)...
-Unlocking achievement CgkI0IOM87sIEAIQQQ (com.grumpyrhinogames.necromerger)...
-Unlocking achievement CgkI0IOM87sIEAIQQg (com.grumpyrhinogames.necromerger)...
-Unlocking achievement CgkI0IOM87sIEAIQQw (com.grumpyrhinogames.necromerger)...
-Unlocking achievement CgkI0IOM87sIEAIQRA (com.grumpyrhinogames.necromerger)...
-Unlocking achievement CgkI0IOM87sIEAIQRQ (com.grumpyrhinogames.necromerger)...
-Unlocking achievement CgkI0IOM87sIEAIQRg (com.grumpyrhinogames.necromerger)...
-Unlocking achievement CgkI0IOM87sIEAIQRw (com.grumpyrhinogames.necromerger)...
-Unlocking achievement CgkI0IOM87sIEAIQSA (com.grumpyrhinogames.necromerger)...
+
+### Nice CLI (simplifies basic usage)
+
+_show all games registered in cc_
+```bash
+python nicecli.py --games
 ```
 
 _show all games not 100% completed_
 ```bash
-python3 nicecli.py --all-games-n
+python nicecli.py --all-games-n
 ```
+
+_unlock single achievement_
+```bash
+python nicecli.py --unlock CgkIoKSYmJkOEAIQ5AE
 ```
-+---------+-------------+----------+--------------+-------+
-| Game ID | Name        | Unlocked | Achievements | In CC |
-+---------+-------------+----------+--------------+-------+
-| 16      | Among Us    | 7        | 33           | No    |
-| 141     | Grimvalor   | 7        | 27           | Yes   |
-| 244     | Plague Inc. | 63       | 200          | Yes   |
-| 245     | Rebel Inc.  | 8        | 71           | No    |
-+---------+-------------+----------+--------------+-------+
+
+_unlock multiple achievements_
+```bash
+python nicecli.py --unlock-list CgkIoKSYmJkOEAIQ5AE CgkIoKSYmJkOEAIQxgE
+```
+
+_unlock all achievements in provided app_
+```bash
+python nicecli.py --unlock-all -g com.miniclip.plagueinc
+```
+
+_and much more..._
+```bash
+python gpau.py --help
+python nicecli.py --help
 ```
--- nicecli.py
+++ nicecli.py
@@ -10,28 +10,39 @@ parser = argparse.ArgumentParser(epilog='By @TheNoiselessNoise')
 parser.add_argument('-i', dest='input', metavar='input', help='path to the .db file')
 parser.add_argument('--games', action='store_true', help='Show all games registered in cc')
 parser.add_argument('--all-games', action='store_true', help='Show all games')
-parser.add_argument('--all-games-n', action='store_true', help='Show all games (not 100%)')
-parser.add_argument('--sort', dest='sort', type=str, help='Sort by specified column name (default: Name)', default="Name")
+parser.add_argument('--all-games-n', action='store_true', help='Show all games not 100%% completed')
+parser.add_argument('--sort', dest='sort', type=str, help='Sort by specified column name, default: Name', default="Name")
 ach_group = parser.add_argument_group('Achievements')
-ach_group.add_argument('--show', action='store_true', help='Show all achievements of specified game')
-ach_group.add_argument('-g', dest='game', type=str, help='Package name or cc id of a game')
+ach_group.add_argument('--info', action='store_true', help='Show info about specified game, needs -g as Game ID')
+ach_group.add_argument('--show', action='store_true', help='Show all achievements of specified game, needs -g as Package Name or Game ID')
+ach_group.add_argument('--unlock', type=str, metavar='ach_ex_id', help='Unlock specified achievement, Achievement external ID')
+ach_group.add_argument('--unlock-list', nargs='+', metavar='ach_ex_id', help='Unlock specified achievements, Achievement external IDs separated by space')
+ach_group.add_argument('--unlock-all', action='store_true', help='Unlock all achievements, needs -g as CC ID')
+ach_group.add_argument('-g', dest='game', type=str, help='Package Name, Game ID or CC ID')
 
 skip_pkgs = ["com.google.android.play.games", "com.google.android.gms"]
 
 class CliDummy:
-    input = None
-    games = False
-    all_games = False
+    input       = None
+    games       = False
+    all_games   = False
     all_games_n = False
-    sort = "Name"
-    show = False
-    game = None
+    sort        = "Name"
 
-def table(data, title = None):
+    info        = False
+    show        = False
+    unlock      = False
+    unlock_list = None
+    unlock_all  = False
+
+    game        = None  # Package Name, Game ID or CC ID
+
+def table(data, title=None):
     t = AsciiTable(data)
     if title is not None:
-        t.title = title
-    print(t.table)
+        print("\n+" + "-" * (len(title) + 2) + "+")
+        print(f"| {title} |")
+    print(("\n" if title is None else "") + t.table + "\n")
 
 def sort_rows(rows, sort: str = None):
     sort = sort.lower()
@@ -47,15 +58,16 @@ def show_games(g: Gpau, sort: str = None):
 
     games = [(g.finder.game_inst_by_package_name(cc.package_name), cc) for cc in ccs if cc.package_name not in skip_pkgs]
 
-    rows = [["CC ID", "Name", "Achievements"]]
+    rows = [["CC ID", "Game ID", "Package Name", "Name", "Achievements"]]
     for ginst, cc in games:
         game = g.finder.game_by_game_inst(ginst)
         ach_defs = g.finder.ach_defs_by_game(game)
         ach_insts = g.finder.ach_insts_by_ach_defs(ach_defs)
         uachs = [x for x in ach_insts if x.is_unlocked()]
 
-        ach_str = f"{len(uachs)}/{len(ach_insts)}" if len(ach_insts) else "--NO--ACHS--"
-        rows.append([cc.id, game.display_name, ach_str])
+        ach_str = f"{len(uachs)}/{len(ach_insts)}" if len(ach_insts) else "-" * len(rows[0][-1])
+        package_name = "NO_INSTANCE" if ginst is None else ginst.package_name
+        rows.append([cc.id, game.id, package_name, game.display_name, ach_str])
 
     table(sort_rows(rows, sort), "Games registered in cc")
 
@@ -63,7 +75,7 @@ def show_all_games(g: Gpau, sort: str = None, not_100: bool = False):
     __start_time = time.time()
     games = [game for game in g.db.select_by_cls(Game)]
 
-    rows = [["Game ID", "Name", "Unlocked", "Achievements", "In CC"]]
+    rows = [["Game ID", "Package Name", "Name", "Unlocked", "Achievements", "In CC"]]
     index = 0
     for game in games:
         index += 1
@@ -85,7 +97,8 @@ def show_all_games(g: Gpau, sort: str = None, not_100: bool = False):
         if not_100 and (not ach_defs or len(uachs) == len(ach_insts)):
             continue
 
-        rows.append([game.id, game.display_name, len(uachs), len(ach_insts), in_cc])
+        package_name = "NO_INSTANCE" if game_inst is None else game_inst.package_name
+        rows.append([game.id, package_name, game.display_name, len(uachs), len(ach_insts), in_cc])
 
     table(sort_rows(rows, sort))
 
@@ -99,42 +112,84 @@ def get_achievement_type(ach_def: AchievementDefinition):
     types.append("Sec" if ach_def.is_secret() else "Nor")
     return "/".join(types)
 
-def show_achs(name, g: Gpau, sort: str = None):
-    game = find_game(name, g)
+def get_achievement_state(ach_def: AchievementDefinition, ach_inst: AchievementInstance):
+    if ach_inst.is_unlocked():
+        return "UNLOCKED"
+    elif ach_def.is_incremental():
+        return f"{ach_inst.current_steps}/{ach_def.total_steps}"
+    return "LOCKED"
+
+def show_achs(g: Gpau, name, sort: str = None):
+    game = find_game(g, name, use_game_id=True)
     if not game:
-        ex(f"Game with name or cc id '{name}' not found")
+        ex(f"Game with Package Name or Game ID '{name}' not found")
 
     game_inst = g.finder.game_inst_by_game(game)
     ach_defs = g.finder.ach_defs_by_game(game)
     ach_insts = g.finder.ach_insts_by_ach_defs(ach_defs)
 
-    rows = [["Type", "Name", "Description", "State"]]
+    rows = [["Type", "External ID", "Name", "Description", "State"]]
 
     if not ach_defs:
         rows.append(["-"*len(x) for x in rows[0]])
 
     for d, i in zip(ach_defs, ach_insts):
-        if i is None:
-            rows.append([get_achievement_type(d), d.name, d.description, "NO_INSTANCE"])
-            continue
+        ach_type = get_achievement_type(d)
+        ach_exid = d.external_achievement_id
+        ach_name = d.name
+        ach_desc = d.description.strip()
+        ach_state = "NO_INSTANCE" if i is None else get_achievement_state(d, i)
 
-        if i.is_unlocked():
-            state = "UNLOCKED"
-        elif d.is_incremental():
-            state = f"{i.current_steps}/{d.total_steps}"
-        else:
-            state = "LOCKED"
-
-        rows.append([get_achievement_type(d), d.name, d.description, state])
+        rows.append([ach_type, ach_exid, ach_name, ach_desc, ach_state])
 
     table(sort_rows(rows, sort), f"{game.display_name} ({game_inst.package_name})")
 
-def find_game(name_or_ccid, g: Gpau):
+def find_game(g: Gpau, name_or_ccid, use_game_id=False):
     if name_or_ccid.isdigit():
+        if use_game_id:
+            gi = g.finder.game_inst_by_game_id(name_or_ccid)
+            return g.finder.game_by_game_inst(gi) if gi else None
+
         cc = g.finder.client_context_by_id(name_or_ccid)
-        return find_game(cc.package_name, g) if cc else None
+        return find_game(g, cc.package_name) if cc else None
+
     gi = g.finder.game_inst_by_package_name(name_or_ccid)
     return g.finder.game_by_game_inst(gi) if gi else None
+
+def unlock_all_achs(g: Gpau, ccid):
+    g.args.auto_inc_achs = True
+
+    game = find_game(g, ccid)
+    if not game:
+        ex(f"Game with Package Name or CC ID '{ccid}' not found")
+
+    ach_defs = g.finder.ach_defs_by_game(game)
+    for ach_def in ach_defs:
+        g.unlock_achievement(ach_def)
+
+def unlock_ach(g: Gpau, ach_external_id):
+    g.args.auto_inc_achs = True
+
+    ach_def = g.finder.ach_def_by_external_id(ach_external_id)
+    if not ach_def:
+        ex(f"Achievement with External ID '{ach_external_id}' not found", _ex=False)
+
+    g.unlock_achievement(ach_def)
+
+def unlock_achs(g: Gpau, ach_external_ids):
+    for ach in ach_external_ids:
+        unlock_ach(g, ach)
+
+def show_game_info(g: Gpau, gid):
+    game = find_game(g, gid, use_game_id=True)
+    if not game:
+        ex(f"Game with Game ID '{gid}' not found")
+    game.dump()
+
+    game_inst = g.finder.game_inst_by_game(game)
+    if not game_inst:
+        ex(f"Game doesn't have any Instance")
+    game_inst.dump()
 
 def main(args):
     args: CliDummy = args
@@ -149,8 +204,16 @@ def main(args):
         show_all_games(g, args.sort)
     elif args.all_games_n:
         show_all_games_n(g, args.sort)
+    elif args.info and args.game:
+        show_game_info(g, args.game)
     elif args.show and args.game:
-        show_achs(args.game, g, args.sort)
+        show_achs(g, args.game, args.sort)
+    elif args.unlock:
+        unlock_ach(g, args.unlock)
+    elif args.unlock_list:
+        unlock_achs(g, args.unlock_list)
+    elif args.unlock_all and args.game:
+        unlock_all_achs(g, args.game)
 
 if __name__ == '__main__':
     if not len(sys.argv[1:]):
